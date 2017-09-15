b50a06f456ece0be7c2ef277e191d3d4658706fd
julien chiquet <julien.chiquet@gmail.com>
2017-09-15 10:30:18 +0200

    added the possibility to pass and initialization to PLN + many option for NLOPT

--- R/PLN.R
+++ R/PLN.R
@@ -14,9 +14,14 @@
 ##'
 ##' @details The parameter \code{control} is a list controlling the optimization with the following entries
 ##' \itemize{
-##'  \item{"xtol"}{stop when an optimization step changes every parameters by less than xtol multiplied by the absolute value of the parameter. Default is 1e-4}
-##'  \item{"ftol"}{stop when an optimization step changes the objective function by less than xtol multiplied by the absolute value of the parameter. Default is 1e-6}
-##'  \item{"maxit"}{stop when the number of iteration exceeds maxiter. Default is 10000}
+##'  \item{"ftol_rel"}{stop when an optimization step changes the objective function by less than ftol multiplied by the absolute value of the parameter. Default is 1e-6}
+##'  \item{"ftol_abs"}{stop when an optimization step changes the objective function by less than ftol multiplied by the absolute value of the parameter. Default is 1e-6}
+##'  \item{"xtol_rel"}{stop when an optimization step changes every parameters by less than xtol multiplied by the absolute value of the parameter. Default is 1e-4}
+##'  \item{"xtol_abs"}{stop when an optimization step changes every parameters by less than xtol multiplied by the absolute value of the parameter. Default is 1e-4}
+##'  \item{"maxeval"}{stop when the number of iteration exceeds maxeval. Default is 10000}
+##'  \item{"method"}{the optimization method used by NLOPT among LD type, i.e. "MMA", "LBFGS",
+##'     "TNEWTON", "TNEWTON_RESTART", "TNEWTON_PRECOND", "TNEWTON_PRECOND_RESTART",
+##'     "TNEWTON_VAR1", "TNEWTON_VAR2". See NLOPTR documentation for further details. Default is "MMA".}
 ##'  \item{"lbvar"}{the lower bound (box constraint) for the variational variance parameters. Default is 1e-5.}
 ##'  \item{"trace"}{integer for verbosity. Useless when \code{cores} > 1}
 ##' }
@@ -54,30 +59,45 @@ PLN.default <- function(Y, X = matrix(1, nrow = nrow(Y)), O = matrix(0, nrow(Y),
   ##
 
   ## define default control parameters for optim and overwrite by user defined parameters
-  ctrl <- list(ftol=1e-6, xtol=1e-4, maxit=10000, lbvar=1e-5, method="MMA", trace=1)
+  ctrl <- list(ftol_rel = 1e-6,  ftol_abs = 1e-4, xtol_rel = 1e-4, xtol_abs = 1e-4, maxeval = 10000, method = "MMA", lbvar = 1e-5, trace = 1, inception = NULL)
   ctrl[names(control)] <- control
 
   ## problem dimensions and constant
   n  <- nrow(Y); p <- ncol(Y); d <- ncol(X)
-  KY <-sum(.logfactorial(Y))
+  ## Initialization if user-specified
+  if (!is.null(ctrl$inception)) {
+    stopifnot(all.equal(class(ctrl$inception), c("PLNfit", "R6")),
+              all.equal(dim(ctrl$inception$model.par$Sigma)  , c(p,p)),
+              all.equal(dim(ctrl$inception$model.par$Theta)  , c(p,d)),
+              all.equal(dim(ctrl$inception$variational.par$M), c(n,p)),
+              all.equal(dim(ctrl$inception$variational.par$S), c(n,p)))
+    if(ctrl$trace > 1) cat("\n User defined inceptive PLN model")
+    par0 <- c(ctrl$inception$model.par$Theta,
+              ctrl$inception$variational.par$M,
+              ctrl$inception$variational.par$S)
+  } else {
+    if(ctrl$trace > 1) cat("\n Using Poisson GLM for initialization")
+    ## glm-Poisson model for the regression parameters
+    Theta <- do.call(rbind, lapply(1:p, function(j) coefficients(glm.fit(X, Y[, j], offset = O[,j], family = poisson()))))
+    ## 0 mean and minimum variance for the variational parameters
+    par0 <- c(Theta, rep(0, n*p), rep(10*ctrl$lbvar, n*p))
+  }
 
   ## ===========================================
   ## OPTIMIZATION
   ##
+  KY <-sum(.logfactorial(Y)) ## constant that will remain the same
   if (ctrl$trace > 0) cat("\n Adjusting the standard PLN model.")
 
-  ## Initialization
-  ## glm-Poisson model for the regression parameters
-  Theta <- do.call(rbind, lapply(1:p, function(j) coefficients(glm.fit(X, Y[, j], offset = O[,j], family = poisson()))))
-  ## 0 mean and minimum variance for the variational parameters
-  par0 <- c(Theta, rep(0, n*p), rep(10*ctrl$lbvar, n*p))
   lower.bound <- c(rep(-Inf, p*d), rep(-Inf, p*n), rep(ctrl$lbvar, n*p))
 
   ## Now optimize with NLOPTR
   opts <- list("algorithm"   = paste("NLOPT_LD",ctrl$method, sep="_"),
-               "maxeval"     = ctrl$maxit,
-               "xtol_rel"    = ctrl$xtol,
-               "ftol_rel"    = ctrl$ftol,
+               "maxeval"     = ctrl$maxeval,
+               "ftol_rel"    = ctrl$ftol_rel,
+               "ftol_abs"    = ctrl$ftol_abs,
+               "xtol_rel"    = ctrl$xtol_rel,
+               "xtol_abs"    = ctrl$xtol_abs,
                "print_level" = max(0,ctrl$trace-1))
 
   optim.out <- nloptr(par0, eval_f = fn_optim_PLN_profiled_Cpp, lb = lower.bound, opts = opts,
