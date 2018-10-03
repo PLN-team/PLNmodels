454c61d71cf24a2968bcef63937097986d065b84
Julien Chiquet <julien.chiquet@inra.fr>
2018-10-03 18:04:25 +0200

    test for PLNmixture on iris data set

---
+++ inst/test/test_PLNMM.R
@@ -0,0 +1,18 @@
+library(PLNmodels)
+library(testthat)
+library(aricode)
+
+data("iris")
+
+lambda <- exp(as.matrix(iris[, 1:4]))
+count <- matrix(rpois (nrow(iris) * 4, lambda), nrow(iris), 4)
+
+myPLN <- PLNMM(count ~ 1, clusters = 2:4)
+
+myPLN$plot(c("loglik", "BIC"))
+
+bestModel <- myPLN$getBestModel('BIC')
+
+aricode::ARI(bestModel$memberships, iris$Species)
+
+pairs(iris[, 1:4], col = bestModel$memberships, pch = as.numeric(iris$Species) )
