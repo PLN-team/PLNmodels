2579930fec747e696f9b10cc5c3e81162d04b8fb
Julien Chiquet <julien.chiquet@inra.fr>
2018-10-08 10:00:21 +0200

    corect links to winlib.R

--- tools/winlib.R
+++ tools/winlib.R
@@ -1,5 +1,6 @@
 # Build against mingw-w64 build of libnlopt2.4.2
-if(!file.exists("../windows/nlopt/include/nlopt.h")){
+if(!file.exists("../windows/nlopt-2.4.2/include/nlopt.h")){
+  if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/nlopt/archive/v2.4.2.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
