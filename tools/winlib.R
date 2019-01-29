87d9320b5e280e574a07bf6e761f0417944e5d33
Julien Chiquet <julien.chiquet@gmail.com>
2019-01-29 08:47:10 +0100

    removing dependecy to curl

--- tools/winlib.R
+++ tools/winlib.R
@@ -1,6 +1,5 @@
 # Build against mingw-w64 build of libnlopt2.4.2
 if(!file.exists("../windows/nlopt-2.4.2/include/nlopt.h")){
-  if(getRversion() < "3.3.0") setInternet2()
   download.file("https://github.com/rwinlib/nlopt/archive/v2.4.2.zip", "lib.zip", quiet = TRUE)
   dir.create("../windows", showWarnings = FALSE)
   unzip("lib.zip", exdir = "../windows")
