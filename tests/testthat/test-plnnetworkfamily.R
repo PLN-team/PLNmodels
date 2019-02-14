21faef9dc19b21ce5db931fb042c31a7cd53776c
mahendra-mariadassou <mahendra.mariadassou@gmail.com>
2019-02-14 14:48:26 +0100

    Do not perform stability selection if a previous one has been detected.

--- tests/testthat/test-plnnetworkfamily.R
+++ tests/testthat/test-plnnetworkfamily.R
@@ -3,10 +3,29 @@ context("test-plnnetworkfamily")
 data(trichoptera)
 trichoptera <- prepare_data(trichoptera$Abundance, trichoptera$Covariate)
 
+data(mollusk)
+mollusc <- prepare_data(mollusk$Abundance, mollusk$Covariate)
+
 test_that("PLNnetwork methods", {
 
   models <- PLNnetwork(Abundance ~ 1, data = trichoptera)
   expect_equal(getBestModel(models), getBestModel(models, "BIC"))
 
   ## add some
+})
+
+test_that("PLNnetwork computes a the stability path only once.", {
+
+  ## Compute network and stability selection once
+  nets <- PLNnetwork(Abundance ~ 0 + site + offset(log(Offset)),
+                     data = mollusc)
+  set.seed(1077)
+  subs <- replicate(2,
+                    sample.int(nrow(mollusc), size = round(nrow(mollusc)/2)),
+                    simplify = FALSE)
+  stability_selection(nets, subsamples = subs)
+  ## try to compute it again
+  expect_message(stability_selection(nets),
+                 "Previous stability selection detected. Use \"force = TRUE\" to recompute it.")
+
 })
