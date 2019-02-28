128c718d676907a3b49b69f1103449a4fb81521d
Julien Chiquet <julien.chiquet@inra.fr>
2019-02-28 15:01:21 +0100

    terrible bug due to inconsistency in mapply, going back to lapplyhtop

--- R/PLNPCAfamily-class.R
+++ R/PLNPCAfamily-class.R
@@ -32,10 +32,8 @@ PLNPCAfamily$set("public", "initialize",
   private$params <- ranks
 
   ## save some time by using a common SVD to define the inceptive models
-  M <- mapply(
-    function(y, o, x, w) {lm.wfit(x, log(1 + y), w, offset =  o)$residuals},
-      data.frame(responses), data.frame(offsets), MoreArgs = list(x = covariates, w = weights)
-    )
+  M <- do.call(cbind, lapply(1:ncol(responses), function(j)
+    residuals(lm.wfit(covariates, log(1 + responses[,j]), w = weights, offset = offsets[, j]))))
   control$svdM <- svd(M, nu = max(ranks), nv = ncol(responses))
 
   ## instantiate as many models as ranks
--- tests/testthat/test-plcpca.R
+++ tests/testthat/test-plcpca.R
@@ -3,12 +3,17 @@ context("test-plcpca")
 ## get data with some offset
 data(trichoptera)
 trichoptera <- prepare_data(trichoptera$Abundance, trichoptera$Covariate)
+data(mollusk)
+mollusc <- prepare_data(mollusk$Abundance, mollusk$Covariate)
 
 test_that("PLNPCA runs", {
 
   models <- PLNPCA(Abundance ~ 1 + offset(log(Offset)), data = trichoptera)
   expect_is(models, "PLNPCAfamily")
-  ## add some...
+
+  models <- PLNPCA(Abundance ~ 1 + offset(log(Offset)), data = mollusc, ranks = 1:8, control_main = list(cores = 8))
+  expect_is(models, "PLNPCAfamily")
+    ## add some...
 })
 
 test_that("PLNPCA is fast on low ranks", {
