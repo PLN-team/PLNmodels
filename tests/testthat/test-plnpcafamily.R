209354fab705fa38e35915326408775129137e9c
Julien Chiquet <julien.chiquet@inra.fr>
2019-02-28 15:33:37 +0100

    testing PLNPCAfamily

--- tests/testthat/test-plnpcafamily.R
+++ tests/testthat/test-plnpcafamily.R
@@ -5,10 +5,27 @@ trichoptera <- prepare_data(trichoptera$Abundance, trichoptera$Covariate)
 
 test_that("PLNPCAfamily methods", {
 
-  models <- PLNPCA(Abundance ~ 1 + offset(log(Offset)), data = trichoptera)
+  models <- PLNPCA(Abundance ~ 1, data = trichoptera)
 
-  ## this is more PLNPCAfamilyfit testing
-  expect_equal(getBestModel(models), getBestModel(models, "ICL"))
-  ## add some...
+  X <- model.matrix(Abundance ~ 1, data = trichoptera)
+  Y <- as.matrix(trichoptera$Abundance)
+  O <- matrix(0, nrow = nrow(Y), ncol = ncol(Y))
+  w <- rep(1, nrow(Y))
+
+  ## extract the data matrices and weights
+  ctrl_init <- PLNmodels:::PLN_param(list(), nrow(Y), ncol(Y), ncol(X), weighted = FALSE)
+  ctrl_main <- PLNmodels:::PLNPCA_param(list(), weighted = FALSE)
+
+  ## instantiate
+  myPLN <- PLNmodels:::PLNPCAfamily$new(1:5, Y, X, O, w, Abundance ~ 1, ctrl_init)
+
+  ## optimize
+  myPLN$optimize(ctrl_main)
+
+  ## post-treatment
+  myPLN$postTreatment()
+
+  expect_equivalent(myPLN, models)
+  ## add some
 })
 
