64fbbea13e8b0607bed7a7837f4cbb0e6fe2c25a
Julien Chiquet <julien.chiquet@inra.fr>
2019-05-09 17:21:54 +0200

    speeding up the test

--- tests/testthat/test-plnnetworkfamily.R
+++ tests/testthat/test-plnnetworkfamily.R
@@ -3,11 +3,6 @@ context("test-plnnetworkfamily")
 data(trichoptera)
 trichoptera <- prepare_data(trichoptera$Abundance, trichoptera$Covariate)
 
-data(mollusk)
-mollusc <- suppressWarnings(
-  prepare_data(mollusk$Abundance, mollusk$Covariate)
-)
-
 test_that("PLNnetwork: main function, fields access and methods", {
 
   models <- PLNnetwork(Abundance ~ 1, data = trichoptera)
@@ -59,14 +54,14 @@ test_that("PLNnetwork: main function, fields access and methods", {
 
 test_that("PLNnetwork computes the stability path only once.", {
   ## Compute network and stability selection once
-  nets <- PLNnetwork(Abundance ~ 0 + site + offset(log(Offset)),
-                     data = mollusc)
+  nets <- PLNnetwork(Abundance ~ 1 + offset(log(Offset)),
+                     data = trichoptera)
   ## extract_probs fails if stability selection has not been performed.
   expect_error(extract_probs(nets),
                "Please perform stability selection using stability_selection(Robject) first", fixed = TRUE)
   set.seed(1077)
   subs <- replicate(2,
-                    sample.int(nrow(mollusc), size = nrow(mollusc)/2),
+                    sample.int(nrow(trichoptera), size = nrow(trichoptera)/2),
                     simplify = FALSE)
   stability_selection(nets, subsamples = subs)
   ## Stability_path has correct dimensions
--- tests/testthat/test-plnpcafamily.R
+++ tests/testthat/test-plnpcafamily.R
@@ -54,13 +54,13 @@ COLLECTION OF 5 POISSON LOGNORMAL MODELS
   fixed = TRUE)
 })
 
-test_that("PLNPCA is fast on low ranks", {
-
-  n <- 100
-  p <- 1000
-  lambda <- exp(rnorm(n * p))
-  Y <- matrix(rpois(n * p, lambda), n, p)
-
-  models <- PLNPCA(Y ~ 1, ranks = 1:3)
-  expect_is(models, "PLNPCAfamily")
-})
+# test_that("PLNPCA is fast on low ranks", {
+#
+#   n <- 100
+#   p <- 1000
+#   lambda <- exp(rnorm(n * p))
+#   Y <- matrix(rpois(n * p, lambda), n, p)
+#
+#   models <- PLNPCA(Y ~ 1, ranks = 1:3)
+#   expect_is(models, "PLNPCAfamily")
+# })
