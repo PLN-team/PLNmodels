b1bc1b3fd66b053458518d277cc254f29eb4460b
Julien Chiquet <julien.chiquet@gmail.com>
2020-05-29 16:54:15 +0200

    fixing #52: vector of penalties now account for penalize_diagonal=FALSE/TRUE

--- R/PLNnetwork.R
+++ R/PLNnetwork.R
@@ -55,7 +55,8 @@ PLNnetwork <- function(formula, data, subset, weights, penalties = NULL, control
   ctrl_init <- PLN_param(control_init, nrow(args$Y), ncol(args$Y), ncol(args$X))
   if (is.null(ctrl_init$nPenalties)) ctrl_init$nPenalties <- 30
   if (is.null(ctrl_init$min.ratio)) ctrl_init$min.ratio   <- .1
-  ctrl_init$penalty_weights <- ctrl_main$penalty_weights
+  ctrl_init$penalty_weights   <- ctrl_main$penalty_weights
+  ctrl_init$penalize_diagonal <- ctrl_main$penalize_diagonal
 
   ## Instantiate the collection of models
   if (ctrl_main$trace > 0) cat("\n Initialization...")
