15de4e4b4906927bd08715c80229620c98593bec
François Gindraud <francois.gindraud@gmail.com>
2021-05-07 17:02:00 +0200

    ZI: outer loop and dimension tests
    Add outer loop of optimize_zi.
    For now, configuration is shared between outer loop and inner nloptr steps.
    Added basic dimensionnality test, also useful as an example of the API.
    
    The test of convergence is complex due to input flexibility.
    It has been split as a standalone function to allow testing.
    
    Thorough testing uncovered a bad assumption with named lists :
    they are a vector<(value,name)> with insertion order, not a dictionnary.
    As the convergence function may be reused elsewhere, it has been
    improved to support any order of named lists at the cost of complexity.
    The alternative would have been to assume same order and tweak
    optimize_zi to get the same ordering of names, but it leads to a brittle
    and unclear API.
    Runtime cost should be low as named lists are used as struct-like
    constructs with fixed (low) size in this context.
    
    step_b solve() problem generates warnings of singular matrix.
    A quick R version errors-out for the same reason.

---
+++ R/PLNzi.R
@@ -0,0 +1,160 @@
+# ZI optimization:
+#
+# init_parameters: named list(Theta, Theta0, Omega, M, S2, Pi)
+# Y, X, O: observed data, constant
+# configuration: named list(algorithm, xtol_rel, xtol_abs, ftol_rel, ftol_abs, maxeval, maxtime)
+#
+# Returns named list(parameters: named list, nb_iter: int, stop_reason: str)
+#
+# Configuration :
+# Forwarded to nloptr iterative steps (c,e,f).
+# Except for algorithm, the keys are optional termination criteria. Not all of them are needed.
+# A missing key or a key set to a negative number means disabled.
+# xtol_abs: double for every parameter, or named list with values for each parameter.
+#
+# xtol_abs, xtol_rel, maxeval are also used for overall loop.
+# ftol_abs and ftol_rel could maybe be used if defined on J(params), but I'm not sure.
+#
+# Dimensions are checked only on C++ side.
+optimize_zi <- function(init_parameters, Y, X, O, configuration) {
+
+    # Prepare configuration structs for steps with nloptr
+    config_for <- function(parameter_name) {
+        # This relies on "copy on write" semantics in R ; configuration is copied when modified
+        if("xtol_abs" %in% names(configuration)) {
+            xtol_abs <- configuration$xtol_abs
+            if(is.double(xtol_abs)) {
+                # double means applying the value for everything.
+                # nloptr wrapper already treats it as such, keep it unchanged.
+            } else if(is.list(xtol_abs)) {
+                # list(param_names = values) means using specific values (double / matrix) for each parameter.
+                # extract value for specific parameter ; nlopt wrapper will check dimensions
+                configuration$xtol_abs <- xtol_abs[[parameter_name]]
+            } else {
+                warning("optimize_zi: configuration$xtol_abs is neither a list or a double")
+            }
+        }
+        configuration
+    }
+    configuration_step_c <- config_for("Theta0")
+    configuration_step_e <- config_for("M")
+    configuration_step_f <- config_for("S2")
+
+    # Follow the convention of maxeval = -1 => disabled
+    maxeval <- if("maxeval" %in% names(configuration)) { configuration$maxeval } else { -1 }
+    
+    # Main loop
+    nb_iter <- 0
+    parameters <- init_parameters
+    repeat {
+        # Check maxeval
+        if(maxeval >= 0 && nb_iter >= maxeval) {
+            return(list(
+                parameters = parameters,
+                nb_iter = nb_iter,
+                stop_reason = "maxeval"
+            ))
+        }
+
+        # Steps
+        new_Omega <- cpp_optimize_zi_step_a(
+            M = parameters$M, X = X, Theta = parameters$Theta, S2 = parameters$S2
+        )
+        new_Theta <- cpp_optimize_zi_step_b(
+            M = parameters$M, X = X
+        )
+        new_Theta0 <- cpp_optimize_zi_step_c(
+            init_Theta0 = parameters$Theta0,
+            X = X, Pi = parameters$Pi,
+            configuration = configuration_step_c
+        )$Theta0
+        new_Pi <- cpp_optimize_zi_step_d(
+            Y = Y, X = X, O = O, M = parameters$M, S2 = parameters$S2, Theta0 = new_Theta0
+        )
+        new_M <- cpp_optimize_zi_step_e(
+            init_M = parameters$M,
+            Y = Y, X = X, O = O, Pi = new_Pi, S2 = parameters$S2, Theta = new_Theta, Omega = new_Omega,
+            configuration = configuration_step_e
+        )$M
+        new_S2 <- cpp_optimize_zi_step_f(
+            init_S2 = parameters$S2,
+            O = O, M = new_M, Pi = new_Pi, Theta = new_Theta, Omega = new_Omega,
+            configuration = configuration_step_f
+        )$S2
+
+        # Check convergence
+        new_parameters <- list(
+            Omega = new_Omega, Theta = new_Theta, Theta0 = new_Theta0,
+            Pi = new_Pi, M = new_M, S2 = new_S2
+        )
+        nb_iter <- nb_iter + 1
+        if(parameter_list_converged(
+            parameters, new_parameters,
+            xtol_abs = configuration$xtol_abs, xtol_rel = configuration$xtol_rel
+        )) {
+            return(list(
+                parameters = parameters,
+                nb_iter = nb_iter,
+                stop_reason = "converged"
+            ))
+        }
+        parameters <- new_parameters
+    }
+}
+
+# R step_b alternative : errors with singular matrix too
+optimize_zi_step_b <- function(M, X) {
+    solve(t(X) %*% X) %*% t(X) %*% M
+}
+
+# Test convergence for a named list of parameters
+# oldp, newp: named list of parameters
+# xtol_rel: double ; negative or NULL = disabled
+# xtol_abs: double (negative or NULL = disabled) or list of double/matrices for each parameter (any negative is disabled)
+# Returns boolean
+parameter_list_converged <- function(oldp, newp, xtol_abs = NULL, xtol_rel = NULL) {
+    # Strategy is to compare each pair of list elements with matching names.
+    # Named lists are just vectors (T,str) using order of insertion.
+    # mapply() is handy to do the pair tests, but it works on the underlying vector order (ignoring names).
+    # So reorder lists by their names to use mapply.
+    stopifnot(is.list(oldp), is.list(newp))
+    oldp <- oldp[order(names(oldp))]
+    newp <- newp[order(names(newp))]
+    stopifnot(all(names(oldp) == names(newp)))
+
+    # Check convergence with xtol_rel if enabled
+    if(is.double(xtol_rel) && xtol_rel > 0) {
+        if(all(mapply(function(o, n) { all(abs(n - o) <= xtol_rel * abs(o)) }, oldp, newp))) {
+            return(TRUE)
+        }
+    }
+
+    # Check convergence with xtol_abs (homogeneous) if enabled
+    if(is.double(xtol_abs) && xtol_abs > 0) {
+        if(all(mapply(function(o, n) { all(abs(n - o) <= xtol_abs) }, oldp, newp))) {
+            return(TRUE)
+        }
+    }
+    
+    # Check convergence with xtol_abs as list(xtol_abs for each param_name)
+    if(is.list(xtol_abs)) {
+        xtol_abs <- xtol_abs[order(names(xtol_abs))]
+        stopifnot(all(names(oldp) == names(xtol_abs)))
+        # Due to the possible presence of NULLs, mapply may return a list. unlist allows all() to operate anyway.
+        if(all(unlist(mapply(
+            function(o, n, tol) {
+                if((is.double(tol) && tol > 0) || is.matrix(tol)) {
+                    all(abs(n - o) <= tol)
+                } else {
+                    NULL # Ignore comparison in outer all()
+                }
+            },
+            oldp, newp, xtol_abs
+        )))) {
+            return(TRUE)
+        }
+    }
+
+    # If no criteria has triggered, indicate no convergence
+    FALSE
+}
