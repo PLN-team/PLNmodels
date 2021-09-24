11363cad9b2a4704c873fad43f4cd10b7319c552
Julien Chiquet <julien.chiquet@gmail.com>
2021-09-24 10:37:28 +0200

    factorizing some c++ code into a utils.h file

---
+++ src/utils.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <RcppArmadillo.h>
+
+inline arma::vec logfact(arma::mat y) {
+    y.replace(0., 1.);
+    return sum(y % log(y) - y + log(8 * pow(y, 3) + 4 * pow(y, 2) + y + 1. / 30.) / 6. + std::log(M_PI) / 2., 1);
+}
+
+inline arma::vec ki(arma::mat y) {
+    arma::uword p = y.n_cols;
+    return -logfact(std::move(y)) + 0.5 * double(p) ;
+}
