2e5553f29a152946dbfe4b2a01c2f61060ea28e4
Julien Chiquet <julien.chiquet@inrae.fr>
2022-12-21 18:53:41 +0100

    update script for variance estimation with new format of regression param

--- inst/check/variance_estimates.R
+++ inst/check/variance_estimates.R
@@ -6,34 +6,35 @@ nb_cores <- 10
 options(future.fork.enable = TRUE)
 
 params <- PLNmodels:::create_parameters()
-Theta <- params$Theta
+B <- t(params$Theta)
 
 ## Extract X
 X <- params$X
 
 ## Extract Y
-Y <- rPLN(n = nrow(X), mu = tcrossprod(X, Theta), Sigma = params$Sigma, depths = params$depths)
+Y <- rPLN(n = nrow(X), mu = X %*% B, Sigma = params$Sigma, depths = params$depths)
 
 data <- prepare_data(Y, X, offset = "none")
 O <- rowSums(Y)
 
+conf <- list(variational_var = TRUE, jackknife = TRUE, bootstrap = nrow(Y))
 future::plan("multicore", workers = nb_cores)
-model <- PLN(Abundance ~ 0 + . + offset(log(O)), data = data, control = PLN_param(jackknife = TRUE, bootstrap = nrow(Y)))
+model <- PLN(Abundance ~ 0 + . + offset(log(O)), data = data, control = PLN_param(config_post = conf))
 future::plan("sequential")
 
-Theta_hat <- coef(model)
-Theta_se_var <- standard_error(model)
-Theta_se_jk <- standard_error(model, "jackknife")
-Theta_se_bt <- standard_error(model, "bootstrap")
+B_hat <- coef(model)
+B_se_var <- standard_error(model)
+B_se_jk <- standard_error(model, "jackknife")
+B_se_bt <- standard_error(model, "bootstrap")
 
 data.frame(
-  Theta = rep(c(Theta), 3),
-  Theta_hat = rep(c(Theta_hat), 3),
-  se = c(Theta_se_var, Theta_se_jk, Theta_se_bt),
-  method = rep(c("variational", "jackknife", "bootstrap"), each = length(c(Theta))) ) %>%
-  ggplot(aes(x = Theta, y = Theta_hat)) +
-  geom_errorbar(aes(ymin = Theta_hat - 2 * se,
-                    ymax = Theta_hat + 2 * se), color = "blue") + facet_wrap(~ method) +
+  B = rep(c(B), 3),
+  B_hat = rep(c(B_hat), 3),
+  se = c(B_se_var, B_se_jk, B_se_bt),
+  method = rep(c("variational", "jackknife", "bootstrap"), each = length(c(B))) ) %>%
+  ggplot(aes(x = B, y = B_hat)) +
+  geom_errorbar(aes(ymin = B_hat - 2 * se,
+                    ymax = B_hat + 2 * se), color = "blue") + facet_wrap(~ method) +
   geom_abline(slope = 1, intercept = 0) + labs(x = "True value", y = "Mean estimate") + theme_bw() -> p
 
 print(p)
