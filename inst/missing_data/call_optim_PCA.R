a26ff5d0ffaf5867ddb508ce55f3942a43a858fd
Julien Chiquet <julien.chiquet@inrae.fr>
2023-03-22 14:52:05 +0100

    example in inst fordirect call to PCA optimizer

--- inst/missing_data/call_optim_PCA.R
+++ inst/missing_data/call_optim_PCA.R
@@ -1,6 +1,7 @@
 library(PLNmodels)
 data("trichoptera")
 trichoptera <- prepare_data(trichoptera$Abundance, trichoptera$Covariate)
+
 Y <- as.matrix(trichoptera$Abundance)
 X <- model.matrix(Abundance ~ 1, data = trichoptera)
 n <- nrow(Y)
@@ -8,6 +9,7 @@ p <- ncol(Y)
 d <- ncol(X) # number of covariates
 q <- 5 # number of PCA components
 O <- matrix(0, nrow = n, ncol = p)
+
 data <- list(Y = Y,
              X = X,
              O = O,
@@ -22,4 +24,3 @@ config <- PLNPCA_param()$config_optim
 
 out <- PLNmodels:::nlopt_optimize_rank(data, params, config)
 
-out$B
