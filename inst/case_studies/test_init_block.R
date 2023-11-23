00385106df027df7ad2d75c613b2edf828baad38
Julien Chiquet <julien.chiquet@inrae.fr>
2023-11-23 10:59:54 +0100

    comapring inti

--- inst/case_studies/test_init_block.R
+++ inst/case_studies/test_init_block.R
@@ -1,33 +1,57 @@
+library(tidyverse)
+library(viridis)
+
+## setting up future for parallelism
+nb_cores <- 10
+options(future.fork.enable = TRUE)
+future::plan("multicore", workers = nb_cores)
 
 ## data
 data(trichoptera)
 trichoptera <- prepare_data(trichoptera$Abundance, trichoptera$Covariate)
 nb_blocks <- 1:15
 
+
 ## Start with PLN to get back latent position
 myPLN <- PLN(Abundance ~ 1,  data = trichoptera)
 
 ## Kmean init on the variaitonal means
 Means <- t(myPLN$var_par$M)
-init_kmeans_means <- lapply(nb_blocks, function(k) {
+init_km_1 <- lapply(nb_blocks, function(k) {
   kmeans(Means, centers = k, nstart = 30)$cl
 })
-blocks1 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
-                    control = PLNblock_param(inception = myPLN, init_cl = init_kmeans_means))
-
-D <- 1 - cov2cor(myPLN$model_par$Sigma)
-init_ward2_cor <- hclust(as.dist(D), method = "complete") %>% cutree(nb_blocks) %>% as.data.frame() %>% as.list()
-blocks2 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
-                    control = PLNblock_param(inception = myPLN, init_cl = init_ward2_cor))
+blocks_km_1 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
+                    control = PLNblock_param(inception = myPLN, init_cl = init_km_1))
 
 Sbar <- colSums(myPLN$var_par$S2)
-D <- sqrt(as.matrix(dist(t(myPLN$var_par$M)^2)) + outer(Sbar,rep(1,myPLN$p)) + outer(rep(1, myPLN$p), Sbar))
-init_kmeans_dist <- lapply(nb_blocks, function(k) {
+# D <- sqrt(as.matrix(dist(t(myPLN$var_par$M)^2)) + outer(Sbar,rep(1,myPLN$p)) + outer(rep(1, myPLN$p), Sbar))
+D <- as.matrix(dist(t(myPLN$var_par$M)^2))
+init_km_2 <- lapply(nb_blocks, function(k) {
   kmeans(D, centers = k, nstart = 30)$cl
 })
-blocks3 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
-                    control = PLNblock_param(inception = myPLN, init_cl = init_kmeans_dist))
 
-init_ward2_dist <- hclust(as.dist(D), method = "ward.D2") %>% cutree(nb_blocks) %>% as.data.frame() %>% as.list()
-blocks4 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
-                    control = PLNblock_param(inception = myPLN, init_cl = init_ward2_dist))
+blocks_km_2 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
+                    control = PLNblock_param(inception = myPLN, init_cl = init_km_2))
+
+init_ward2_1 <- hclust(as.dist(D), method = "ward.D2") %>% cutree(nb_blocks) %>% as.data.frame() %>% as.list()
+blocks_hc_1 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
+                    control = PLNblock_param(inception = myPLN, init_cl = init_ward2_1))
+
+D <- 1 - cov2cor(myPLN$model_par$Sigma)
+init_ward2_2 <- hclust(as.dist(D), method = "complete") %>% cutree(nb_blocks) %>% as.data.frame() %>% as.list()
+blocks_hc_2 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
+                    control = PLNblock_param(inception = myPLN, init_cl = init_ward2_2))
+
+init_ward2_3 <- hclustvar(myPLN$var_par$M) %>% cutree(nb_blocks) %>% as.data.frame() %>% as.list()
+blocks_hc_3 <- PLNblock(Abundance ~ 1,  data = trichoptera, nb_blocks = 1:15,
+                        control = PLNblock_param(inception = myPLN, init_cl = init_ward2_3))
+
+bind_rows(bind_cols(blocks_km_1$criteria, method = "kmeans on M"),
+          bind_cols(blocks_km_2$criteria, method = "kmeans on Euclidian distances on M "),
+          bind_cols(blocks_hc_1$criteria, method = "Ward D2 on Euclidian distances"),
+          bind_cols(blocks_hc_2$criteria, method = "Ward D2 on correlation"),
+          bind_cols(blocks_hc_3$criteria, method = "ClustOfVar")) %>%
+  group_by(method) %>%
+  ggplot() + aes(x = param, y = loglik, colour = method) + geom_line() + scale_color_viridis_d() + theme_bw()
+
+future::plan("sequential")
