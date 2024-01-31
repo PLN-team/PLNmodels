fb826d73d79243eb2276c35094ecdb20dc206770
jea312 <jeanne.tous@gmail.com>
2024-01-31 17:08:10 +0100

    Coded all cpp functions for plnblockbis but dimension errors remaining

---
+++ R/optimize_plnblockbis.R
@@ -0,0 +1,129 @@
+# PLNBLOCK optimization:
+#
+# init_parameters: named list(B, M, S, Tau)
+# Y, X, O: observed data, constant
+# configuration: named list(algorithm, xtol_rel, xtol_abs, ftol_rel, ftol_abs, maxeval, maxtime)
+#
+# Returns named list(parameters: named list, nb_iter: int, stop_reason: str)
+#
+# Configuration :
+# Forwarded to nloptr iterative steps (c,e,f).
+# Except for algorithm, the keys are optional termination criteria. Not all of them are needed.
+# A missing key or a key set to a negative number means disabled.
+# xtol_abs: double for every parameter, or named list with values for each parameter.
+#
+# xtol_abs, xtol_rel, maxeval are also used for overall loop.
+# ftol_abs and ftol_rel could maybe be used if defined on J(params), but I'm not sure.
+#
+# Dimensions are checked only on C++ side.
+optimize_plnblockbis <- function(data, params, config) {
+
+  # Link to the approximate function to optimize Omega, depending on the target structure
+  optim_plnblockbis_Omega <- ifelse(is.null(params$rho),
+        optim_plnblockbis_Omega,
+        function(M, S) optim_plnblockbis_Omega_sparse(M, S, data$w, rho = config$rho)
+    )
+  # Main loop
+  nb_iter <- 0
+  parameters <- params; parameters$Omega <- diag(1, ncol(params$M), ncol(params$M))
+  parameters$T <- parameters$Tau; parameters$Tau <- NULL
+  new_parameters <- parameters
+  posteriorProb <- list(new_parameters$T)
+  criterion <- vector("numeric", config$maxit_out)
+  objective <- Inf
+  repeat {
+
+    optim_Omega <- optim_plnblockbis_Omega(M = new_parameters$M, S = new_parameters$S, w = data$w)
+    new_parameters$Omega <- optim_Omega$Omega
+    optim_D <- optim_plnblockbis_D(X = data$X, B = new_parameters$B, Mu=new_parameters$Mu,
+                                   Delta=new_parameters$Delta, w = data$w)
+    new_parameters$D <- optim_D$D
+
+    # VE Step
+    optim_VE <- optim_plnblockbis_VE(data, new_parameters, config)
+    new_parameters$M <- optim_VE$M
+    new_parameters$S <- optim_VE$S
+    new_parameters$Mu <- optim_VE$Mu
+    new_parameters$Delta <- optim_VE$Delta
+    if (!config$fixed_cl) new_parameters$T <- optim_plnblockbis_Tau(data, new_parameters)
+
+    # M Step
+    optim_B <- optim_plnblockbis_B(data, new_parameters, config)
+    new_parameters$B <- optim_B$B
+
+    # Going next step and assessing convergence
+    nb_iter <- nb_iter + 1
+    criterion[nb_iter] <- new_objective <- - plnblockbis_loglik(data, new_parameters)
+
+    objective_converged <-
+      abs(new_objective - objective)/abs(new_objective) < config$ftol_out
+    parameters_converged <- parameter_list_converged(
+      parameters, new_parameters,
+      xtol_abs = config$xtol_abs, xtol_rel = config$xtol_rel
+    )
+    maxit_reached <- config$maxit_out >= 0 && nb_iter >= config$maxit_out
+
+    if (parameters_converged | objective_converged | maxit_reached) {
+      if (parameters_converged) statut <- 4
+      if (objective_converged)  statut <- 3
+      if (maxit_reached) statut <- 5
+      break
+    }
+    posteriorProb <- append(posteriorProb, list(new_parameters$T))
+    parameters <- new_parameters
+    objective  <- new_objective
+  }
+  out   <- new_parameters
+  out$A <- exp(data$O + data$X %*% out$B) * (exp(out$M + .5 * out$S**2) %*% out$T)
+  out$Z <- data$O + data$X %*% out$B + out$M %*% out$T
+  out$Sigma <- optim_Omega$Sigma
+  out$Ji <- plnblockbis_vloglik(data, new_parameters)
+  attr(out$Ji, "weights") <- data$w
+  out$monitoring <- list(
+    objective  = criterion[1:nb_iter],
+    outer_iterations = nb_iter,
+    status     = statut,
+    posteriorProb = posteriorProb,
+    backend = "nlopt-vem"
+  )
+  out
+}
+
+#' @importFrom glassoFast glassoFast
+optim_plnblockbis_Omega_sparse <- function(M, S, w, rho) {
+  n <- sum(w); p <- ncol(M)
+  glassoFast::glassoFast( crossprod(M)/n + diag(crossprod(w, (S * S)), p, p), rho = rho )$wi
+}
+
+# Test convergence for a named list of parameters
+# oldp, newp: named list of parameters
+# xtol_rel: double ; negative or NULL = disabled
+# xtol_abs: double (negative or NULL = disabled) or list of double/matrices for each parameter (any negative is disabled)
+# Returns boolean
+parameter_list_converged <- function(oldp, newp, xtol_abs = NULL, xtol_rel = NULL) {
+  # Strategy is to compare each pair of list elements with matching names.
+  # Named lists are just vectors (T,str) using order of insertion.
+  # mapply() is handy to do the pair tests, but it works on the underlying vector order (ignoring names).
+  # So reorder lists by their names to use mapply.
+  stopifnot(is.list(oldp), is.list(newp))
+  oldp <- oldp[order(names(oldp))]
+  newp <- newp[order(names(newp))]
+  stopifnot(all(names(oldp) == names(newp)))
+
+  # Check convergence with xtol_rel if enabled
+  if(is.double(xtol_rel) && xtol_rel > 0) {
+    if(all(mapply(function(o, n) { all(abs(n - o) <= xtol_rel * abs(o)) }, oldp, newp))) {
+      return(TRUE)
+    }
+  }
+
+  # Check convergence with xtol_abs (homogeneous) if enabled
+  if(is.double(xtol_abs) && xtol_abs > 0) {
+    if(all(mapply(function(o, n) { all(abs(n - o) <= xtol_abs) }, oldp, newp))) {
+      return(TRUE)
+    }
+  }
+
+  # If no criteria has triggered, indicate no convergence
+  FALSE
+}
---
+++ src/optim_plnblockbis.cpp
@@ -0,0 +1,264 @@
+#include <RcppArmadillo.h>
+
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(nloptr)]]
+
+#include "nlopt_wrapper.h"
+#include "packing.h"
+#include "utils.h"
+
+// [[Rcpp::export]]
+arma::vec plnblockbis_vloglik(
+    const Rcpp::List & data  , // List(Y, X, O, w)
+    const Rcpp::List & params  // List(M, S, T, B, Omega)
+) {
+  const arma::mat & Y = Rcpp::as<arma::mat>(data["Y"]); // responses (n,p)
+  const arma::mat & X = Rcpp::as<arma::mat>(data["X"]); // covariates (n,d)
+  const arma::mat & O = Rcpp::as<arma::mat>(data["O"]); // offsets (n,p)
+  const arma::vec & w = Rcpp::as<arma::vec>(data["w"]); // weights (n)
+  const arma::mat & B = Rcpp::as<arma::mat>(params["B"]); // (n,q)
+  const arma::mat & M = Rcpp::as<arma::mat>(params["M"]); // (n,q)
+  const arma::mat & S = Rcpp::as<arma::mat>(params["S"]); // (n,q)
+  const arma::mat & Mu = Rcpp::as<arma::mat>(params["Mu"]); // (n,p)
+  const arma::mat & Delta = Rcpp::as<arma::mat>(params["Delta"]); // (n,p)
+  const arma::mat &T  = Rcpp::as<arma::mat>(params["T"]); // (n,p)
+  const arma::mat & Omega = Rcpp::as<arma::mat>(params["Omega"]); // (q,q)
+  const arma::mat & D = Rcpp::as<arma::mat>(params["D"]); // (p,p)
+  const arma::vec log_pi = arma::trunc_log(mean(T,1));
+
+  const arma::uword q = M.n_cols;
+  const arma::mat S2 = S % S ;
+  const arma::mat XB = X * B ;
+  const arma::mat Delta2 = Delta % Delta ;
+  const arma::mat mr = O + Mu ;
+  const arma::mat Z = mr + M * T;
+  const arma::mat A = trunc_exp(mr + .5 * Delta) % (trunc_exp(M + .5 * S2) * T) ;
+  const arma::mat Dm = inv_sympd(D);
+
+  // Element-wise log-likelihood
+  // rq: j'ai laissé tomber les constantes
+  return(
+    0.5 * real(log_det(Omega)) - logfact(Y) + sum(Y % Z - A, 1)
+    - 0.5 * real(log_det(D)) - accu(T % arma::trunc_log(T))
+    + accu(log_pi.t() * T)
+    + 0.5 * sum(log(S2), 1) + 0.5 * sum(log(Delta2), 1)
+    - 0.5 * sum( (M * Omega) % M + S2 * diagmat(Omega), 1)
+    - 0.5 * sum( (Mu * Omega) % Mu + Delta2 * Dm, 1)
+    - 0.5 * sum(XB % Dm % XB.t(), 1)
+    + sum(XB % Dm % (XB.t()), 1)
+    + sum(Mu % Dm % (XB.t()), 1)
+  ) ;
+
+}
+
+// [[Rcpp::export]]
+arma::vec plnblockbis_loglik(
+    const Rcpp::List & data  , // List(Y, X, O, w)
+    const Rcpp::List & params  // List(M, S, T, B, Omega)
+) {
+  const arma::mat & Y = Rcpp::as<arma::mat>(data["Y"]); // responses (n,p)
+  const arma::mat & X = Rcpp::as<arma::mat>(data["X"]); // covariates (n,d)
+  const arma::mat & O = Rcpp::as<arma::mat>(data["O"]); // offsets (n,p)
+  const arma::vec & w = Rcpp::as<arma::vec>(data["w"]); // weights (n)
+  const arma::mat & B = Rcpp::as<arma::mat>(params["B"]); // (n,q)
+  const arma::mat & M = Rcpp::as<arma::mat>(params["M"]); // (n,q)
+  const arma::mat & S = Rcpp::as<arma::mat>(params["S"]); // (n,q)
+  const arma::mat & Mu = Rcpp::as<arma::mat>(params["Mu"]); // (n,p)
+  const arma::mat & Delta = Rcpp::as<arma::mat>(params["Delta"]); // (n,p)
+  const arma::mat &T  = Rcpp::as<arma::mat>(params["T"]); // (n,p)
+  const arma::mat & Omega = Rcpp::as<arma::mat>(params["Omega"]); // (q,q)
+  const arma::mat & D = Rcpp::as<arma::mat>(params["D"]); // (p,p)
+  const arma::vec log_pi = arma::trunc_log(mean(T,1));
+
+  const arma::uword q = M.n_cols;
+  const arma::mat S2 = S % S ;
+  const arma::mat XB = X * B ;
+  const arma::mat Delta2 = Delta % Delta ;
+  const arma::mat mr = O + Mu ;
+  const arma::mat Z = mr + M * T;
+  const arma::mat A = trunc_exp(mr + .5 * Delta2) % (trunc_exp(M + .5 * S2) * T) ;
+  const arma::mat Dm = inv_sympd(D);
+
+  // Element-wise log-likelihood
+  // rq: j'ai laissé tomber les constantes
+  return(
+    0.5 * real(log_det(Omega)) - logfact(Y) + sum(Y % Z - A, 1)
+    - 0.5 * real(log_det(D)) - accu(T % arma::trunc_log(T))
+    + accu(log_pi.t() * T)
+    + 0.5 * sum(log(S2), 1) + 0.5 * sum(log(Delta2), 1)
+    - 0.5 * sum( (M * Omega) % M + S2 * diagmat(Omega), 1)
+    - 0.5 * sum( (Mu * Omega) % Mu + Delta2 * Dm, 1)
+    - 0.5 * sum(XB % Dm % XB.t(), 1)
+    + sum(XB % Dm % (XB.t()), 1)
+    + sum(Mu % Dm % (XB.t()), 1)
+  ) ;
+}
+
+// [[Rcpp::export]]
+Rcpp::List  optim_plnblockbis_Omega(
+  const arma::mat & M, // (n,q)
+  const arma::mat & S, // (n,q)
+  const arma::vec & w  // (n)
+) {
+  double w_bar = accu(w);
+  arma::mat nSigma = (M.t() * (M.each_col() % w) + diagmat(w.t() * (S % S))) ;
+
+  return Rcpp::List::create(
+    Rcpp::Named("Sigma") = (1./w_bar) * nSigma,
+    Rcpp::Named("Omega") = w_bar * inv_sympd(nSigma));
+}
+
+
+// [[Rcpp::export]]
+Rcpp::List  optim_plnblockbis_B(
+    const Rcpp::List & data  , // List(Y, X, O, w)
+    const Rcpp::List & params,  // List(M, S, T, B, Omega)
+    const arma::vec & w  // (n)
+) {
+  const arma::mat & X = Rcpp::as<arma::mat>(data["X"]); // covariates (n,d)
+  const arma::mat & Mu = Rcpp::as<arma::mat>(params["Mu"]); // (n,p)
+  double w_bar = accu(w);
+  arma::mat B = inv_sympd((X.t() * w * X)) * X.t() * w * Mu;
+  return Rcpp::List::create(
+    Rcpp::Named("B") = B);
+}
+
+// [[Rcpp::export]]
+Rcpp::List  optim_plnblockbis_D(
+    const arma::mat & X,
+    const arma::mat & B,
+    const arma::mat & Mu,
+    const arma::mat & Delta,
+    const arma::vec & w  // (n)
+) {
+  const arma::mat Mu2 = Mu % Mu ;
+  const arma::mat Delta2 = Delta % Delta ;
+  const arma::mat XB = X * B ;
+
+  double w_bar = accu(w);
+  arma::vec d = sum(Mu2 + Delta2 + XB - 2 * (Mu % XB) , 1);
+  arma::mat nD = diagmat(d);
+
+  return Rcpp::List::create(
+    Rcpp::Named("D") = (1./w_bar) * nD);
+}
+
+// [[Rcpp::export]]
+arma::mat optim_plnblockbis_Tau(
+    const Rcpp::List & data  , // List(Y, X, O, w)
+    const Rcpp::List & params  // List(M, S, T, B, Omega)
+) {
+  const arma::mat & Y = Rcpp::as<arma::mat>(data["Y"]); // responses (n,p)
+  const arma::mat & X = Rcpp::as<arma::mat>(data["X"]); // covariates (n,d)
+  const arma::mat & O = Rcpp::as<arma::mat>(data["O"]); // offsets (n,p)
+  const arma::mat & B = Rcpp::as<arma::mat>(params["B"]); // (n,q)
+  const arma::mat & M = Rcpp::as<arma::mat>(params["M"]); // (n,q)
+  const arma::mat & S = Rcpp::as<arma::mat>(params["S"]); // (n,q)
+  const arma::mat & T = Rcpp::as<arma::mat>(params["T"]); // (q,p)
+  const arma::mat & Omega = Rcpp::as<arma::mat>(params["Omega"]); // (q,q)
+  const arma::mat & Mu = Rcpp::as<arma::mat>(params["Mu"]); // (n,p)
+  const arma::mat & Delta = Rcpp::as<arma::mat>(params["Delta"]); // (n,p)
+  const arma::mat Delta2 = Delta % Delta ;
+  const arma::vec log_pi = arma::trunc_log(mean(T,1));
+  const arma::mat mr = O + Mu ;
+  const arma::mat A1 = trunc_exp(mr + .5 * Delta2) ;
+  const arma::mat A2 = trunc_exp(M + .5 * S % S ) ;
+
+  arma::mat Tau = M.t() * Y - A1.t() * A2  ;
+  Tau.each_col() += log_pi ;
+  Tau.each_col( [](arma::vec& x){
+    x = trunc_exp(x - max(x)) / sum(trunc_exp(x - max(x))) ;
+  }) ;
+  return Tau ;
+}
+
+// [[Rcpp::export]]
+Rcpp::List optim_plnblockbis_VE(
+    const Rcpp::List & data  , // List(Y, X, O, w)
+    const Rcpp::List & params, // List(M, S, T, B, Omega)
+    const Rcpp::List & configuration // List of config values
+) {
+  const arma::mat & Y = Rcpp::as<arma::mat>(data["Y"]); // responses (n,p)
+  const arma::mat & X = Rcpp::as<arma::mat>(data["X"]); // covariates (n,d)
+  const arma::mat & O = Rcpp::as<arma::mat>(data["O"]); // offsets (n,p)
+  const arma::vec & w = Rcpp::as<arma::vec>(data["w"]); // weights (n)
+  const arma::mat & B = Rcpp::as<arma::mat>(params["B"]); // (n,q)
+  const arma::mat & T = Rcpp::as<arma::mat>(params["T"]); // (q,p)
+  const arma::mat & Omega = Rcpp::as<arma::mat>(params["Omega"]); // (q,q)
+  const auto init_M   = Rcpp::as<arma::mat>(params["M"]); // (n,q)
+  const auto init_S   = Rcpp::as<arma::mat>(params["S"]); // (n,q)
+  const auto init_Mu   = Rcpp::as<arma::mat>(params["Mu"]); // (n,q)
+  const auto init_Delta   = Rcpp::as<arma::mat>(params["Delta"]); // (n,q)
+  const auto metadata = tuple_metadata(init_M, init_S, init_Mu, init_Delta);
+  enum { M_ID, S_ID, Mu_ID, Delta_ID }; // Names for metadata indexes
+
+  auto parameters = std::vector<double>(metadata.packed_size);
+  metadata.map<M_ID>(parameters.data()) = init_M;
+  metadata.map<S_ID>(parameters.data()) = init_S;
+  metadata.map<Mu_ID>(parameters.data()) = init_Mu;
+  metadata.map<Delta_ID>(parameters.data()) = init_Delta;
+  auto optimizer = new_nlopt_optimizer(configuration, parameters.size());
+  set_uniform_xtol_abs(optimizer.get(), Rcpp::as<double>(configuration["xtol_abs"]));
+
+  const arma::mat mr = O + X * B ;
+  const arma::mat A2 = trunc_exp(mr) ;
+  const arma::mat XB = X * B ;
+
+  // Optimize
+  auto objective_and_grad = [&metadata, &Y, &X, &O, &mr, &A2,  &T, &Omega, &w, &XB](const double * params, double * grad) -> double {
+    const arma::mat M = metadata.map<M_ID>(params);
+    const arma::mat S = metadata.map<S_ID>(params);
+    const arma::mat Mu = metadata.map<Mu_ID>(params);
+    const arma::mat Delta = metadata.map<Delta_ID>(params);
+
+    arma::mat Delta2 = Delta % Delta ;
+    arma::mat S2 = S % S ;
+    arma::mat Mu2 = Mu % Mu ;
+    arma::mat A1  = trunc_exp(O + Mu + .5 * Delta2) ;
+    arma::mat A2  = trunc_exp(M + .5 * S2) ;
+    arma::mat A   = A1 % (A2 * T) ;
+    arma::mat A_T = A2 % (A1 * T.t()) ;
+    arma::mat nSigma = M.t() * (M.each_col() % w) + diagmat(w.t() * S2);
+    arma::vec d = sum(Mu2 + Delta2 + XB - 2 * (Mu % XB) , 1);
+    arma::mat nD = diagmat(d);
+    double w_bar = accu(w);
+    arma::mat nDm = diagmat(1./d);
+    arma::mat Dm = diagmat((1./w_bar) * 1./d);
+    std::cout << "4!" << std::endl;
+    // C'est ici qu'il y a un problème
+    //Error: matrix multiplication: incompatible matrix dimensions: 17x17 and 49x49
+    // Q = 17 dans cette situation je pense
+    // Le problème vient du terme E
+    std::cout << "E" << std::endl;
+    double t5 = 0.5 * trace(Mu.t() * (Mu.each_col() % w) * nDm);
+    std::cout << "F" << std::endl;
+    double t6 = 0.5 * trace(diagmat(w.t() * S2) * nDm);
+
+    double objective =
+      accu(w.t() * (A - Y % (O + Mu + M * T))) - 0.5 * accu(w.t() * log(S2))
+      - 0.5 * accu(w.t() * log(Delta2)) + .5 * trace(Omega * nSigma)
+      + 0.5 * trace(Mu.t() * (Mu.each_col() % w) * nDm)
+      + 0.5 * trace(diagmat(w.t() * S2) * nDm);
+    std::cout << "5!" << std::endl;
+    metadata.map<M_ID>(grad) = diagmat(w) * (M * Omega + A_T - Y * T.t());
+    metadata.map<S_ID>(grad) = diagmat(w) * (S.each_row() % diagvec(Omega).t() + S % A_T - pow(S, -1));
+    metadata.map<Mu_ID>(grad) = Y - Mu * Omega - A_T;
+    metadata.map<Delta_ID>(grad) = diagmat(w) * (Delta.each_row() % diagvec(Dm).t() + (A2 * T) % A1 % Delta - pow(Delta, -1));
+
+    return objective;
+  };
+  OptimizerResult result = minimize_objective_on_parameters(optimizer.get(), objective_and_grad, parameters);
+
+    arma::mat M = metadata.copy<M_ID>(parameters.data());
+    arma::mat S = metadata.copy<S_ID>(parameters.data());
+    arma::mat Mu = metadata.copy<Mu_ID>(parameters.data());
+    arma::mat Delta = metadata.copy<Delta_ID>(parameters.data());
+    return Rcpp::List::create(
+      Rcpp::Named("status") = static_cast<int>(result.status),
+      Rcpp::Named("iterations") = result.nb_iterations,
+      Rcpp::Named("objective") = result.objective,
+      Rcpp::Named("M") = M,
+      Rcpp::Named("S") = S,
+      Rcpp::Named("Mu") = Mu,
+      Rcpp::Named("Delta") = Delta);
+}
