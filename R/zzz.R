d6b0d1a02e343c43a93d6bd0792948d9901d061c
Julien Chiquet <julien.chiquet@inrae.fr>
2024-03-12 10:07:14 +0100

    correcting typo in zzz.R (on load message)

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
     version <- read.dcf(file=system.file("DESCRIPTION", package=pkgname), fields="Version")
-    packageStartupMessage(paste0("This is packages '", pkgname,"' version ",version))
+    packageStartupMessage(paste0("This is package '", pkgname,"' version ",version))
     packageStartupMessage('Use future::plan(multicore/multisession) to speed up PLNPCA/PLNmixture/stability_selection.')
 }
