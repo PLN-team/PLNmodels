5d82dff7c7b1b4fbb3a04d0c66f445487d8f606a
Julien Chiquet <julien.chiquet@inrae.fr>
2024-11-22 12:23:43 +0100

    C++ working version of sandwich

--- R/PLNfit-class.R
+++ R/PLNfit-class.R
@@ -239,24 +239,9 @@ PLNfit <- R6Class(
     },
 
     vcov_sandwich_B = function(Y, X) {
-      getMat_iCnB <- function(i) {
-        a_i <- as.numeric(private$A[i, ])
-        s_i <- as.numeric(private$S[i, ])
-        omega <- as.numeric(diag(private$Omega))
-        diag_mat_i <- diag(1/a_i + s_i^4 / (1 + s_i^2 * (a_i + omega)))
-        solve(private$Sigma + diag_mat_i)
-      }
-      YmA <- Y - private$A
-      Dn <- matrix(0, self$d*self$p, self$d*self$p)
-      Cn <- matrix(0, self$d*self$p, self$d*self$p)
-      for (i in 1:self$n) {
-        xxt_i <- tcrossprod(X[i, ])
-        Cn <- Cn - kronecker(getMat_iCnB(i) , xxt_i) / self$n
-        Dn <- Dn + kronecker(tcrossprod(YmA[i,]), xxt_i) / self$n
-      }
-      Cn_inv <- solve(Cn)
-      dim_names <- dimnames(attr(private$B, "vcov_variational"))
-      vcov_sand <- ((Cn_inv %*% Dn %*% Cn_inv) / self$n) %>% `dimnames<-`(dim_names)
+      vcov_sand <- get_sandwich_variance_B(Y, X, private$A,
+                                           private$S, private$Sigma, diag(private$Omega)
+      )
       attr(private$B, "vcov_sandwich") <- vcov_sand
       attr(private$B, "variance_sandwich") <- matrix(diag(vcov_sand), nrow = self$d, ncol = self$p,
                                                      dimnames = dimnames(private$B))
--- src/utils-R.cpp
+++ src/utils-R.cpp
@@ -4,6 +4,8 @@
 
 #include "utils.h"
 
+using namespace arma ;
+
 // [[Rcpp::export]]
 arma::mat get_sandwich_variance_B(
   const arma::mat & Y,
@@ -14,51 +16,24 @@ arma::mat get_sandwich_variance_B(
   const arma::vec & Diag_Omega
 ) {
 
-  arma::uword n = Y.n_rows ;
-  arma::uword p = Y.n_cols ;
-  arma::uword d = X.n_cols ;
+  const uword n = Y.n_rows ;
+  const uword p = Y.n_cols ;
+  const uword d = X.n_cols ;
 
-  auto get_iCnB = [&A, &S, &Diag_Omega, &Sigma](arma::uword i) {
-    arma::vec a = A.row(i) ;
-    arma::vec s = S.row(i) ;
-    arma::mat D = diagmat(pow(a, -1) + pow(s, 4) / (1 + pow(s,2) * (a + Diag_Omega))) ;
+  mat Cn(d*p, d*p, fill::zeros) ;
+  mat Dn(d*p, d*p, fill::zeros) ;
 
-    return arma::inv_sympd(Sigma + D);
-  };
+  mat D = pow(A, -1) + pow(S, 4) / (1 + square(S) % (A + ones(n) * Diag_Omega.t())) ;
+  mat YmA = Y - A ;
 
-  arma::mat YmA = Y - A ;
-  arma::mat Cn = arma::zeros(d*p, d*p) ;
-  arma::mat Dn = arma::zeros(d*p, d*p) ;
-  for (int i=0; i<n; i++) {
-    arma::mat xxt_i = X.col(i) * X.col(i).t() ;
-    arma::mat yyt_i = YmA.col(i) * YmA.col(i).t() ;
-    Cn = Cn - arma::kron(get_iCnB(i), xxt_i) / n ;
-    Dn = Dn + arma::kron(yyt_i, xxt_i) / n ;
+  for (uword i=0; i<n; i++) {
+    mat xxt_i = X.row(i).t() * X.row(i) ;
+    mat yyt_i = YmA.row(i).t() * YmA.row(i) ;
+    Cn -= kron(inv_sympd(Sigma + diagmat(D.row(i))), xxt_i) / n ;
+    Dn += kron(yyt_i, xxt_i) / n ;
   }
 
-  arma::mat Cn_inv = arma::inv_sympd(Cn) ;
+  mat Cn_inv = inv(Cn) ;
 
   return (Cn_inv * Dn * Cn_inv) / n ;
 }
-
-// vcov_sandwich_B = function(Y, X) {
-//  getMat_iCnB <- function(i) {
-//     a_i <- as.numeric(private$A[i, ])
-//     s_i <- as.numeric(private$S[i, ])
-//     omega <- as.numeric(diag(private$Omega))
-//     diag_mat_i <- diag(1/a_i + s_i^4 / (1 + s_i^2 * (a_i + omega)))
-//     solve(private$Sigma + diag_mat_i)
-//  }
-//
-//  YmA <- Y - private$A
-//    Dn <- matrix(0, self$d*self$p, self$d*self$p)
-//    Cn <- matrix(0, self$d*self$p, self$d*self$p)
-//    for (i in 1:self$n) {
-//    xxt_i <- tcrossprod(X[i, ])
-//    Cn <- Cn - kronecker(getMat_iCnB(i) , xxt_i) / self$n
-//    Dn <- Dn + kronecker(tcrossprod(YmA[i,]), xxt_i) / self$n
-//  }
-//   Cn_inv <- solve(Cn)
-//     dim_names <- dimnames(attr(private$B, "vcov_variational"))
-//     vcov_sand <- ((Cn_inv %*% Dn %*% Cn_inv) / self$n) %>% `dimnames<-`(dim_names)
-//
